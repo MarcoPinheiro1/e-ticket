a4d94726e1be3a5325beb74e6da9e6f5ada82f3e
Marco Pinheiro <marcopinheirodev@gmail.com>
2024-09-23 20:14:24 -0300

    verificando Quantidade digitada

--- js/app.js
+++ js/app.js
@@ -1,6 +1,11 @@
 function comprar() {
     let setorIngresso = document.getElementById('tipo-ingresso').value;
     let quantidade = document.getElementById('qtd').value;
+    
+    if (isNaN(qtd) || qtd <= 0) {
+        alert('Quantidade inválida! Insira Novamente.');
+        return;
+    }
 
     if (setorIngresso == 'pista') {
         comprarPista(quantidade);
@@ -14,32 +19,32 @@ function comprar() {
 function comprarPista(quantidade) {
     let qtdPista = parseInt(document.getElementById('qtd-pista').textContent);
     if (qtdPista < qtd) {
-        alert('Quantidade indisponível para tipo pista');
+        alert('Quantidade indisponível para o setor Pista!');
     } else {
         qtdPista = qtdPista - quantidade;
         document.getElementById('qtd-pista').textContent = qtdPista;
-        alert('Compra realizada com sucesso!');
+        alert('Parabéns! Compra realizada com sucesso!');
     }
 }
 
 function comprarSuperior(quantidade) {
     let qtdSuperior = parseInt(document.getElementById('qtd-superior').textContent);
     if (qtdSuperior < quantidade) {
-        alert('Quantidade indisponível para tipo superior');
+        alert('Quantidade indisponível para o setor superior');
     } else {
         qtdSuperior = qtdSuperior - quantidade;
         document.getElementById('qtd-superior').textContent = qtdSuperior;
-        alert('Compra realizada com sucesso!');
+        alert('Parabéns! Compra realizada com sucesso!');
     }
 }
 
 function comprarInferior(quantidade) {
     let qtdInferior = parseInt(document.getElementById('qtd-inferior').textContent);
     if (qtdInferior < quantidade) {
-        alert('Quantidade indisponível para tipo inferior');
+        alert('Quantidade indisponível para o setor inferior');
     } else {
         qtdInferior = qtdInferior - quantidade;
         document.getElementById('qtd-inferior').textContent = qtdInferior;
-        alert('Compra realizada com sucesso!');
+        alert('Parabéns! Compra realizada com sucesso!');
     }
 }
